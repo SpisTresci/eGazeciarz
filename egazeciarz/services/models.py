d121d6451ff52870577355a760dde32e112ee147
Patrick Perduta <perdutapatrick@gmail.com>
2014-05-20 15:11:18 +0200

    repair services app, ugly code

--- egazeciarz/services/models.py
+++ egazeciarz/services/models.py
@@ -3,7 +3,4 @@ from django.db import models
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
-
-
-class User(models.Model):
-    pass  # service_id = models.
+    status = models.BooleanField(default=False)
--- egazeciarz/services/views.py
+++ egazeciarz/services/views.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 from services.models import Service
@@ -12,8 +11,14 @@ def index(request):
 
 def detail(request, service_id):
     service = get_object_or_404(Service, id=service_id)
-    return render(request, 'services/detail.html', {'service': service})
+    return render(request, 'services/detail.html', {'service': service, 'checkbox': not service.status or "checked"})
 
 
 def save(request, service_id):
     service = get_object_or_404(Service, id=service_id)
+    try:
+        service.status = request.POST['status']
+    except(KeyError):
+        service.status = False
+    service.save()
+    return detail(request, service_id)
