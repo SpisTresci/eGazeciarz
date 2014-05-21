7f59c671d0632797b4383d12245237ec4c311494
Patrick Perduta <perdutapatrick@gmail.com>
2014-05-21 15:33:05 +0200

    reworked, beauty code

--- egazeciarz/services/models.py
+++ egazeciarz/services/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=100)
     status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- egazeciarz/services/views.py
+++ egazeciarz/services/views.py
@@ -1,30 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.views import generic
 
 from services.models import Service
 
 
-def index(request):
-    services_list = Service.objects.all()
-    context = {'services_list': services_list}
-    return render(request, 'services/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'services/index.html'
+    context_object_name = 'services_list'
+
+    def get_queryset(self):
+        return Service.objects.all()
 
 
-def detail(request, service_id):
-    service = get_object_or_404(Service, id=service_id)
-    return render(
-        request,
-        'services/detail.html',
-        {
-            'service': service,
-            'checkbox': not service.status or 'checked'
-        })
+class DetailView(generic.DetailView):
+    template_name = 'services/detail.html'
+    model = Service
 
 
 def save(request, service_id):
     service = get_object_or_404(Service, id=service_id)
-    try:
-        service.status = request.POST['status']
-    except(KeyError):
+    if 'status' in request.POST:
+        service.status = True
+    else:
         service.status = False
     service.save()
-    return detail(request, service_id)
+    return HttpResponseRedirect(reverse('services:detail', args=service_id))
