500f0f1891528359e4a425c00463d947f10ccd10
John Smith <Anonym@anon.ym>
2014-06-21 18:10:57 +0200

    T599: added STATIC_DIRS and TEMPLATE_DIRS to settings

--- egazeciarz/egazeciarz/settings.py
+++ egazeciarz/egazeciarz/settings.py
@@ -81,3 +81,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Template files
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "static", "templates")
+)
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
+)
+
--- egazeciarz/egazeciarz/urls.py
+++ egazeciarz/egazeciarz/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,3 +11,6 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^services/', include('services.urls', namespace='services')),
 )
+
+urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
+
