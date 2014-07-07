e41450d02775910f30d4623cfd1e30f5966f656c
Patryk Perduta <pperduta@spistresci.pl>
2014-07-07 16:48:14 +0200

    Auditors: kszumny, msmolik T597: FIX: true wsgi.py and base.html for testing

--- egazeciarz/egazeciarz/urls.py
+++ egazeciarz/egazeciarz/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^services/', include('services.urls', namespace='services')),
+    url(r'^$', TemplateView.as_view(template_name='egazeciarz/base.html'))
 )
 
 if settings.DEBUG:
