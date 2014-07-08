953ee4eb227eb01f67468ed556fe2bb4958eca84
Magdalena Szumna <magdalena@ekundelek.pl>
2014-07-08 11:12:02 +0200

    T608: WIP - home page
    Summary:
    T608: Strona Główna
    
    Wygląd: {M12}
    
    Reviewers: msmolik, pperduta, kszumny
    
    Reviewed By: pperduta, kszumny
    
    Maniphest Tasks: T608
    
    Differential Revision: https://dev.spistresci.pl/D40

--- egazeciarz/egazeciarz/urls.py
+++ egazeciarz/egazeciarz/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="home"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^services/', include('services.urls', namespace='services')),
