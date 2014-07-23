eeddfbb404c591d844cce33c9c587ee5475159b1
msmolik <dyzajash@gmail.com>
2014-07-23 18:58:46 +0200

    Zaimplementować model bazy danych w oparciu o przygotowany schemat
    Summary: pep8 fix
    
    Reviewers: pperduta, kszumny
    
    Reviewed By: kszumny
    
    Subscribers: astachecki
    
    Maniphest Tasks: T609
    
    Differential Revision: https://dev.spistresci.pl/D53

--- egazeciarz/services/admin.py
+++ egazeciarz/services/admin.py
@@ -1,4 +1,33 @@
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 from django.contrib import admin
-from services.models import Service
+from models import (
+    Days,
+    Hours,
+    ReceiverType,
+    Service,
+    SubPattern,
+    Subscriptions,
+    UserProfile,
+    UserSubs,
+)
 
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+
+class UserProfileAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), UserProfileAdmin)
+admin.site.register(ReceiverType)
+admin.site.register(Hours)
+admin.site.register(Days)
+admin.site.register(Subscriptions)
+admin.site.register(UserSubs)
+admin.site.register(SubPattern)
 admin.site.register(Service)
--- egazeciarz/services/models.py
+++ egazeciarz/services/models.py
@@ -1,4 +1,82 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class ReceiverType(models.Model):
+    receiver = models.CharField(max_length=50, unique=True)
+
+    def __unicode__(self):
+        return self.receiver
+
+
+class Hours(models.Model):
+    hours = models.TimeField(unique=True)
+
+    def __unicode__(self):
+        return u'%s' % self.hours
+
+
+class Days(models.Model):
+    dow = models.CharField(max_length=10, unique=True)
+
+    def __unicode__(self):
+        return self.dow
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        unique=True,
+        related_name='profile',
+    )
+    receiver_type = models.ManyToManyField(
+        ReceiverType,
+        null=True,
+        blank=True,
+    )
+
+    def display_receiver(self):
+        return ','.join([
+            ReceiverType.receiver
+            for ReceiverType in self.receiver_type.all()
+        ])
+
+    display_receiver.short_description = 'Receiver types'
+    display_receiver.allow_tags = True
+
+    def __unicode__(self):
+        return "Profil: %s" % self.user
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, created = UserProfile.objects.get_or_create(user=instance)
+
+
+class Subscriptions(models.Model):
+    visable_name = models.CharField(max_length=50, unique=True)
+    reciper_name = models.CharField(max_length=50)
+    website_url = models.URLField()
+
+    def __unicode__(self):
+        return self.visable_name
+
+
+class UserSubs(models.Model):
+    subscriptions = models.ForeignKey(Subscriptions, to_field='visable_name')
+    user = models.ForeignKey(User, to_field='username')
+
+    def __unicode__(self):
+        return u"%s" % self.id
+
+
+class SubPattern(models.Model):
+    sub_id = models.ForeignKey(UserSubs)
+    day_id = models.ForeignKey(Days, to_field='dow')
+    hour_id = models.ForeignKey(Hours, to_field='hours')
+
+    def __unicode__(self):
+        return u"%s" % self.id
 
 
 class Service(models.Model):
