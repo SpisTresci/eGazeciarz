fe367deaa068d9cdb5706125a5189a3b93630aab
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2014-09-28 23:42:42 +0200

    Zintegrować MailCatchera ze środowiskiem testowym eGazeciarza
    Reviewers: kszumny
    
    Reviewed By: kszumny
    
    Subscribers: pperduta, astachecki, kszumny
    
    Maniphest Tasks: T583
    
    Differential Revision: https://dev.spistresci.pl/D63

--- egazeciarz/egazeciarz/settings/staging.py
+++ egazeciarz/egazeciarz/settings/staging.py
@@ -1,6 +1,22 @@
 from default import *
+try:
+    from local_settings import (
+        EMAIL_HOST_USER as USER,
+        EMAIL_HOST_PASSWORD as PASSWORD,
+    )
+
+except ImportError:
+    print "Can't find local_settings.py"
+
 
 DEBUG = False
+
+if not DEBUG:
+    EMAIL_HOST = '127.0.0.1'
+    EMAIL_HOST_USER = USER
+    EMAIL_HOST_PASSWORD = PASSWORD
+    EMAIL_PORT = 1025
+    EMAIL_USE_TLS = False
 
 TEMPLATE_DEBUG = False
 
