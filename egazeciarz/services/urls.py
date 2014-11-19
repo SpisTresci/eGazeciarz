0a9040113386595531213e17605f95d295a3704b
Magdalena Szumna <magdalena@ekundelek.pl>
2014-11-19 10:22:44 +0100

    T637: Services thumbnails view
    Summary:
    T637: WIP more RWD
    T637: WIP services title tooltip ellipsis
    T637: services thumbnails view
    
    Reviewers: kniski, astachecki, pperduta
    
    Reviewed By: pperduta
    
    Subscribers: kszumny
    
    Maniphest Tasks: T637
    
    Differential Revision: https://dev.spistresci.pl/D72

--- egazeciarz/services/urls.py
+++ egazeciarz/services/urls.py
@@ -4,7 +4,7 @@ from services import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView, name='index'),
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<service_id>\d+)/save$', views.save, name='save'),
 )
--- egazeciarz/services/views.py
+++ egazeciarz/services/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -6,12 +7,97 @@ from django.views import generic
 from services.models import Service
 
 
-class IndexView(generic.ListView):
-    template_name = 'services/index.html'
-    context_object_name = 'services_list'
+from django.shortcuts import render
 
-    def get_queryset(self):
-        return Service.objects.all()
+context = {
+    'sections': [
+        {
+            'name': 'Polskie',
+            'services': [
+                {
+                    'cover': 'http://i.imgur.com/Ei4tfbo.png',
+                    'title': u'Newsweek Polska',
+                },
+                {
+                    'cover': 'http://i.imgur.com/qn51KVR.png',
+                    'title': u'Gazeta Wyborcza',
+                },
+                {
+                    'cover': 'http://i.imgur.com/kfbAP6E.png',
+                    'title': u'Niebezpiecznik.pl',
+                },
+                {
+                    'cover': 'http://i.imgur.com/1N7wlti.png',
+                    'title': u'Czytania na każdy dzień',
+                },
+                {
+                    'cover': 'http://i.imgur.com/5IACxjT.png',
+                    'title': u'eKundelek.pl',
+                },
+                {
+                    'cover': 'http://i.imgur.com/1Pcdry3.png',
+                    'title': u'Bash.org.pl',
+                },
+                {
+                    'cover': 'http://i.imgur.com/4UG050N.png',
+                    'title': u'Bankier.pl',
+                },
+                {
+                    'cover': 'http://i.imgur.com/4hXCAjx.png',
+                    'title': u'Gazeta Prawna',
+                },
+            ],
+        },
+        {
+            'name': 'Angielskie',
+            'services': [
+                {
+                    'cover': 'http://i.imgur.com/Ei4tfbo.png',
+                    'title': u'Newsweek',
+                },
+                {
+                    'cover': 'http://i.imgur.com/Ei4tfbo.png',
+                    'title': u'New York Times',
+                },
+                {
+                    'cover': 'http://i.imgur.com/Ei4tfbo.png',
+                    'title': u'Linux Weekly News',
+                },
+                {
+                    'cover': 'http://i.imgur.com/Ei4tfbo.png',
+                    'title': u'Slashdot',
+                },
+            ],
+        },
+        {
+            'name': 'Angielskie (UK)',
+            'services': [],
+        },
+        {
+            'name': 'Hiszpańskie',
+            'services': [],
+        },
+        {
+            'name': 'Niemieckie',
+            'services': [],
+        },
+        {
+            'name': 'Program TV',
+            'services': [],
+        },
+        {
+            'name': 'Inne',
+            'services': [],
+        },
+    ],
+}
+
+# context zdefiniowany na potrzeby frontendu
+# TODO: context powinien być generowany dynamicznie
+
+
+def IndexView(request):
+    return render(request, 'services/index.html', context)
 
 
 class DetailView(generic.DetailView):
